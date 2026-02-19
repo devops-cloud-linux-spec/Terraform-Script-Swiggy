889d8dff6503cfa3c0a7474deef5df6cd10760f8
Prashik Khobragade <pdevopsjob@gmail.com>
2026-02-19 19:57:56 +0530

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -5,13 +5,17 @@ echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.n
 sudo apt update -y
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-
+sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
 #install docker
 sudo apt-get update
 sudo apt-get install docker.io -y
